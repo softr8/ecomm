75f3b5d6fa3aabc6c066a9c06d45f0033b50adf9
Edwin Cruz <edwin.cruz@crowdint.com>
2011-11-24 12:49:18 -0600

    Initial skeleton

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.2'
+
+gem 'mysql2'
+gem 'spree', git: 'https://github.com/spree/spree.git', branch: '0-70-stable'
+
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 3.1.5.rc.2'
+  gem 'coffee-rails', '~> 3.1.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ecomm::Application.config.secret_token = '0d4cdf9162673806895262885615fad78999348c8d8b99402607f94d72dde2ca0c30d7f57b2281d1a112931f2e72b54c20eaea435a7cd929f976463e5096904e'
